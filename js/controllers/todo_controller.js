da5caa89fcac38b8711031ca868e6a364aa0d268
Will Mitchell <wvmitchell@gmail.com>
2014-01-25 16:01:30 -0700

    todos may now be deleted

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -13,6 +13,12 @@ Todos.TodoController = Ember.ObjectController.extend({
       } else {
         this.get('model').save();
       }
+    },
+
+    removeTodo: function(){
+      var todo = this.get('model')
+      todo.deleteRecord();
+      todo.save();
     }
   },
 
