987159c4b071695ce485089f17d6aac99d2d246d
Will Mitchell <wvmitchell@gmail.com>
2014-01-25 15:55:34 -0700

    edits now persist in model

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -3,6 +3,16 @@ Todos.TodoController = Ember.ObjectController.extend({
   actions: {
     editTodo: function(){
       this.set("isEditing", true);
+    },
+
+    acceptChanges: function(){
+      this.set("isEditing", false);
+
+      if(Ember.isEmpty(this.get('model.title'))){
+        this.send('removeTodo');
+      } else {
+        this.get('model').save();
+      }
     }
   },
 
