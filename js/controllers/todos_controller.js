fac71d5860b30786de43964b8167ebd5374f62af
Will Mitchell <wvmitchell@gmail.com>
2014-01-25 17:37:56 -0700

    can now toggle all todos between complete and active

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -38,8 +38,14 @@ Todos.TodosController = Ember.ArrayController.extend({
     return this.get('completed') > 0;
   }.property('completed'),
 
-  areAllDone: function(){
-    return !!this.get('length') && this.everyProperty('isCompleted', true);
+  areAllDone: function(key, value){
+    if(value == undefined){
+      return !!this.get('length') && this.everyProperty('isCompleted', true);
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
   }.property('@each.isCompleted')
 
 });
